2ad3583923007610a475e71df19de6701e20d0b8
reloadeem <140199914+reloadeem@users.noreply.github.com>
2023-07-21 20:28:11 +0200

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,1 +1,10 @@
-404: Not Found
+        function checkPassword() {
+            var password1 = "streamkiontop";
+            var input = document.getElementById("password").value;
+            
+            if (input === password1) {
+                window.location.href = "ogladaj4.html"; // Przekierowanie do strony z transmisją
+            } else {
+                document.getElementById("error-message").style.display = "block";
+            }
+        }
